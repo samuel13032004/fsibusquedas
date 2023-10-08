289bde216ccdaceb578e110ee2f07411dd17bd57
Luna <hernandezguerraluna@gmail.com>
2023-10-08 18:37:55 +0100

    visited and generated nodes count with repetition, both searches

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ import search
 ab = search.GPSProblem('A', 'B'
                        , search.romania)
 
+print("\nStarting breadth search")
 print("Breadth search", search.breadth_first_graph_search(ab).path())
-#print("Depth search: ", search.depth_first_graph_search(ab).path())
-#print("Breadth search, number of generated nodes: ", len(search.breadth_first_graph_search(ab).path()))
-#print("Depth search, number of visited nodes: ", len(search.depth_first_graph_search(ab).path()))
+print("\nStarting depth search")
+print("Depth search: ", search.depth_first_graph_search(ab).path())
 
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
